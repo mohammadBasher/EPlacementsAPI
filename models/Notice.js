ecd1a2c289bca44e8c140dea9e95ce23839e0b11
Ujjwal Kumar <ujjwalkumar8577@gmail.com>
2021-10-21 18:15:49 +0530

    created new Notice model

---
+++ models/Notice.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+
+const notice = new mongoose.Schema({
+    title:{
+        type: String,
+        required: true
+    },
+    content:{
+        type : String,
+        required : true
+    },
+    timestamp:{
+        type: Number,
+        default: new Date().getTime()
+    }
+});
+
+module.exports = new mongoose.model('notice', notice);
